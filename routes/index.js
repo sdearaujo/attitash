86b2d83bc843a1938104fbd56c37922662dc5431
Anthony Battaglia <atbattaglia@yahoo.com>
2013-03-02 12:32:03 -0500

    added in bootstrap, /home page

--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,10 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
+
+exports.home = function(req, res){
+  res.render('home', { 
+  	title: 'Attitash',
+  	username: 'AttitashDev'
+  });
+};
