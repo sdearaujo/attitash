b4759b288e8095f35699f360f463067643a79d1c
Anthony Battaglia <atbattaglia@yahoo.com>
2013-03-06 20:34:06 -0500

    people to follow, news feed genration done using ejs loop

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,37 @@ exports.home = function(req, res){
   res.render('home', { 
   	title: 'Attitash',
   	user: 'AttitashDev',
-  	userName: 'AttitashDev'
+  	userName: 'AttitashDev',
+  	tashs: [
+  		{	img_src: "images/windows.png",
+  			name: "Windows",
+  			username: "Windows", 
+  			tash_text: "Windows 8 is off to a worse start than Vista! #letsgo #metro"
+  		},
+  		{	img_src: "images/twitter.png",
+  			name: "Twitter",
+  			username: "Twitter", 
+  			tash_text: "Attitash has stolen all of our users! #ohno #attitash"	
+
+  		}
+  	],
+  	who_to_follow: [
+  		{	img_src: "images/george.jpg",
+  			name: "George Costanza",
+  			username: "Costanza", 
+  			followed_by: "Windows"
+  		},
+  		{	img_src: "images/kramer.jpg",
+  			name: "Cosmo Kramer",
+  			username: "K-Man", 
+  			followed_by: "Jerry Seinfeld"
+  		},
+  		{	img_src: "images/sein.jpg",
+  			name: "Jerry Seinfeld",
+  			username: "Seinfeld", 
+  			followed_by: "Twitter"
+  		}
+  	]
   });
 };
 
