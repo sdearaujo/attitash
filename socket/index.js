f331d8dc467d0254e6c6e89405e328f995664dd4
sdearaujo <sdearaujo@student.umass.edu>
2013-04-14 19:28:13 -0400

    fixing problems

--- socket/index.js
+++ socket/index.js
@@ -1,13 +1,14 @@
-var tashDB = require('./lib/tashs.js');
+var tashDB = require('../lib/tashs.js');
 
 exports.init = function (socket) {
-    socket.on('new tash', function (data) {
+    	socket.on('new tash', function (data) {
 		client = JSON.stringify(data);
 		tashDB.addTash(client.username, client.text, new Date().toISOString(), function(err){
-		if(err){
-			io.sockets.emit('new tash error', "error creating tash!");
-		}else{
-			io.sockets.emit('new tash success', data);
-		}
-    });
-});
+			if(err){
+				io.sockets.emit('new tash error', "error creating tash!");
+			}else{
+				io.sockets.emit('new tash success', data);
+			}
+    		});
+	});
+}
